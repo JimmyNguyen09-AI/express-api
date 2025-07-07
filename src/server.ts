c9596b0eb589136ed22ee6217391ce18ee1305b8
JimmyNguyen09-AI <ng.trungthanh04@gmail.com>
2025-07-07 16:41:42 +1000

    af

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://zora-frontend-alpha.vercel.app/',
+    origin: 'https://zora-frontend-alpha.vercel.app',
     credentials: true
 }))
 
