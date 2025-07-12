6f282cebfa13e91df07b974d4ccfdbbd4eafedf8
JimmyNguyen09-AI <ng.trungthanh04@gmail.com>
2025-07-12 15:10:06 +1000

    aaac

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import authRoutes from "./auth/auth.route";
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors({
     origin: "https://zora-frontend-alpha.vercel.app",
@@ -15,4 +15,4 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => console.log(`Server running on port http://0.0.0.0:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://127.0.0.1:${PORT}`));
